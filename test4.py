c553a59203bc114d4f36128a11ce001f296d1b40
JiangRenbin <1451144759@qq.com>
2017-05-10 18:59:42 +0800

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,22 @@
+import urllib.request
+import re
+import os
+
+
+def fetch_pictures(url):
+    html_content = urllib.request.urlopen(url).read()
+    r = re.compile('<img class="j-pl-photoitem-img" style="(.*?)" data-width="172" data-height="129" data-bwidth="1440" data-bheight="1080" data-cpx="0" src="(.*?)" data-loaded="1" data-oriwidth="200"')
+    picture_url_list = r.findall(html_content.decode('utf-8'))
+
+    os.mkdir('liushishi')
+    os.chdir(os.path.join(os.getcwd(), 'liushishi'))
+    for i in range(len(picture_url_list)):
+        picture_name = str(i) + '.jpg'
+        try:
+            urllib.request.urlretrieve(picture_url_list[i], picture_name)
+            print("Success to download " + picture_url_list[i])
+        except:
+            print("Fail to download " + picture_url_list[i])
+
+if __name__ == '__main__':
+    fetch_pictures("https://user.qzone.qq.com/2219352323/4")
